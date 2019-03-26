014cbb329c700759aba9c8f5328d7a08f26870eb
Terkwood <metaterkhorn@gmail.com>
2019-03-26 09:38:03 -0400

    rename script and methods

--- DeepZIndexHack.gd
+++ DeepZIndexHack.gd
@@ -1,16 +1,16 @@
 extends Node
 
 func _ready():
-    deep_ysort_everything(self)
+    deep_zindex_hack(self)
 
-func deep_ysort_everything(node):
+func deep_zindex_hack(node):
     for N in node.get_children():
         if N.get_child_count() > 0:
-            deep_ysort_everything(N)
+            deep_zindex_hack(N)
         else:
             if N is Sprite:
-                set_z_order(N)
+                set_zindex(N)
 
-func set_z_order(N):
+func set_zindex(N):
 	var g = N.get_global_transform().get_origin().y + N.region_rect.size.y / 2 * N.scale.y 
 	N.z_index=g
