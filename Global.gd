2e12478808c5ea30854885fe9adef932b018d428
Terkwood <metaterkhorn@gmail.com>
2019-03-26 19:17:47 -0400

    set world size

--- Global.gd
+++ Global.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const TILE_SIZE = 32
-export var world_width = 20    # in tiles
-export var world_height = 20   # in tiles
+export var world_width = 64    # in tiles
+export var world_height = 64   # in tiles
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
