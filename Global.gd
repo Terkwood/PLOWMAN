713b3cb722f98ba35f812f29c7e3b3266e1929c8
Terkwood <metaterkhorn@gmail.com>
2019-03-26 05:54:49 -0400

    Always place corn inside world 🌽
    Squashed commit of the following:
    
    commit 07dabc3ccb5ce92d9e2e86d488382932e619c012
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Tue Mar 26 05:54:40 2019 -0400
    
        place corn inside world
    
    commit 5ff142328ac784546aecb7b710d8b3f4f5cac8ee
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Tue Mar 26 05:42:24 2019 -0400
    
        fathom corn
    
    commit c2754e0b74a300599c6291b4cc6ab3d90275fd9f
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Tue Mar 26 05:17:15 2019 -0400
    
        limit corn placement

--- Global.gd
+++ Global.gd
@@ -7,7 +7,3 @@ export var world_height = 30   # in tiles
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- ProcCorn.gd
+++ ProcCorn.gd
@@ -1,16 +1,5 @@
 extends Node2D
 
-# in tiles
-func max_offset_x(w, corn_size_x):
-	return int((Global.world_width - w) * Global.TILE_SIZE / corn_size_x)
-# in tiles  
-func max_offset_y(h, corn_size_y):
-	return int((Global.world_height - h) * Global.TILE_SIZE / corn_size_y)
-var min_width  =   1     # in tiles
-var max_width  =  20     # in tiles
-var min_height =   1     # in tiles
-var max_height =  20     # in tiles
-
 onready var auto_corn = preload("res://AutoCorn.tscn")
 onready var auto_corn2 = preload("res://AutoCorn2.tscn")
 onready var auto_corn3 = preload("res://AutoCorn3.tscn")
@@ -18,21 +7,37 @@ onready var auto_corn4 = preload("res://AutoCorn4.tscn")
 onready var auto_corn_young = preload("res://AutoCornYoung.tscn")
 	
 onready var corns = [auto_corn, auto_corn2, auto_corn3, auto_corn4, auto_corn_young]
+onready var corn_tile_size = auto_corn4.instance().get_node("Sprite").get_region_rect().size
+
+const min_width  =   1     # in tiles
+var max_width  =  int(min(20,Global.world_width * 0.33))      # in tiles
+const min_height =   1     # in tiles
+var max_height =  int(min(20, Global.world_height * 0.33))     # in tiles
+
+
+# in tiles
+func max_offset_x(w):
+	var rem = Global.TILE_SIZE / corn_tile_size.x  * Global.world_width - w
+	return int(rem * corn_tile_size.x)
+# in tiles  
+func max_offset_y(h):
+	var rem = Global.TILE_SIZE / corn_tile_size.y  * Global.world_height - h
+	return int(rem * corn_tile_size.y )
+
 
 func make_corn():
-	var width = min_width + randi()%max_width
-	var height = min_height + randi()%max_height
-	
-	var tile_size = auto_corn.instance().get_node("Sprite").get_region_rect().size
-	var offset_x = randi()%max_offset_x(width, tile_size.x)
-	var offset_y = randi()%max_offset_y(height, tile_size.y)
-
+	var width = max(min_width, randi()%max_width)
+	var height = max(min_height, randi()%max_height)
+	var offset_x = randi()%max_offset_x(width)
+	var offset_y = randi()%max_offset_y(height)
 	var rand_corn = corns[randi()%corns.size()]
 	for x in range(width):
 		for y in range(height):
 			var corn = rand_corn.instance()
 			add_child(corn)
-			corn.position = Vector2((x + offset_x) * tile_size.x, (y + offset_y) * tile_size.y)
+			corn.position = Vector2(
+				(x * corn_tile_size.x + offset_x) ,
+				(y * corn_tile_size.y + offset_y) )
 			
 func _ready():
 	make_corn()
