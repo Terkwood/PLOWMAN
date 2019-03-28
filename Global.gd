12a3ea0d3bc53778b1bf5cf154c6a4e971b877fc
Terkwood <metaterkhorn@gmail.com>
2019-03-28 05:48:23 -0400

    set flags

--- Global.gd
+++ Global.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const TILE_SIZE = 32
-export var world_width = 64    # in tiles
-export var world_height = 64   # in tiles
+export var world_width = 12    # in tiles
+export var world_height = 12   # in tiles
 
 func _ready():
 	randomize()
