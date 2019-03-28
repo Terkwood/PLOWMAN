e65ca350f46d1656269b6773b0f5708b979701e8
Terkwood <metaterkhorn@gmail.com>
2019-03-28 06:10:54 -0400

    randomly generate a single plant

---
+++ ProcPlants.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+onready var potato = preload("res://AutoPotato.tscn")
+onready var tomato = preload("res://AutoTomato.tscn")
+
+onready var plants = [potato, tomato]
+
+func _ready():
+	var plant = plants[randi()%plants.size()]
+	add_child(plant.instance())
+	
