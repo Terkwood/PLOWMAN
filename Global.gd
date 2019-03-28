d394921ae2442826adc74a37297ef45b62e39298
Terkwood <metaterkhorn@gmail.com>
2019-03-28 09:21:08 -0400

    happy taters

--- Global.gd
+++ Global.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const TILE_SIZE = 32
-export var world_width = 64    # in tiles
-export var world_height = 64   # in tiles
+export var world_width = 40    # in tiles
+export var world_height = 40   # in tiles
 
 func _ready():
 	randomize()
