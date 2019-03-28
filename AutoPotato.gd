4b808440fa8cfc7de8f99986acd97458b3e5b0a1
Terkwood <metaterkhorn@gmail.com>
2019-03-28 09:18:39 -0400

    give the taters a parent

--- AutoPotato.gd
+++ AutoPotato.gd
@@ -12,14 +12,14 @@ onready var sprite_size = mature.instance().get_node("Sprite").get_region_rect()
 const TILE_BUFFER = Vector2(8,8)
 onready var tile_size = Vector2(sprite_size.x + TILE_BUFFER.x, sprite_size.y + TILE_BUFFER.y)
 
-export var size = Vector2(128,128)
+var size = Vector2(128,128)
 
 func _init(s: Vector2):
 	size = s
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("potatos with size " + str(size))
+	print("potatoes with size " + str(size))
 	var zone = ProcZoneRepo.assign_zone(size)
 	var num_potatoes_x = max(0,floor(zone.size.x / tile_size.x) - 1)
 	var num_potatoes_y = max(0,floor(zone.size.y / tile_size.y) - 1)
@@ -31,5 +31,5 @@ func _ready():
 			var potato = stage.instance()
 			potato.position.x = zone.position.x + x * tile_size.x
 			potato.position.y = zone.position.y + y * tile_size.y
-			add_child(potato)
+			get_parent().add_child(potato)
 	
