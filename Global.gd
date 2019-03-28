856fa9d3fcc2b5910600faf4f8bc8f5c23cf830b
Terkwood <metaterkhorn@gmail.com>
2019-03-28 04:59:43 -0400

    Revert world size

--- Global.gd
+++ Global.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const TILE_SIZE = 32
-export var world_width = 15    # in tiles
-export var world_height = 15   # in tiles
+export var world_width = 64    # in tiles
+export var world_height = 64   # in tiles
 
 func _ready():
 	randomize()
