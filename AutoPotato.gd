f76fd76ec5e5325c54e43c7ae3ebc0b590c79bc6
Terkwood <metaterkhorn@gmail.com>
2019-03-28 05:59:49 -0400

    instance a potato at random

--- AutoPotato.gd
+++ AutoPotato.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+onready var young = preload("res://PotatoYoung.tscn")
+onready var growing = preload("res://PotatoGrowing.tscn")
+onready var growing2 = preload("res://PotatoGrowing2.tscn")
+onready var mature = preload("res://PotatoMature.tscn")
+onready var harvested = preload("res://PotatoHarvested.tscn")
+onready var produce = preload("res://PotatoProduce.tscn")
 
+onready var stages = [young, growing, growing2, mature, harvested, produce]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var stage = stages[randi()%stages.size()]
+	add_child(stage.instance())
+	
