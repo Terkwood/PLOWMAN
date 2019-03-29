043c0d4d728cdfc251f8ca561c9149d518affd3a
Terkwood <38859656+Terkwood@users.noreply.github.com>
2019-03-29 15:42:49 -0400

    Use a unified approach for WorldBorder and FencedCow (#36)
    * Tweak position
    
    * add AutoFence.gd
    
    * add an autofence
    
    * procgen
    
    * use generic logic for world border
    
    * trim logic
    
    * some fence
    
    * position the fence
    
    * Rename scene
    
    * add note

---
+++ AutoFence.gd
@@ -0,0 +1,75 @@
+extends Node2D
+
+export var size = Vector2(128,128)
+
+onready var horizontal_fence = preload("res://HorizontalFence.tscn")
+onready var vertical_fence = preload("res://VerticalFence.tscn")
+
+	
+onready var horizontal_fence_tile_size = (
+horizontal_fence.instance().get_node("Sprite").get_region_rect().size
+)
+
+onready var vertical_fence_tile_size = (
+vertical_fence.instance().get_node("Sprite").get_region_rect().size
+)
+
+func num_horizontal_tiles(size_x):
+	return (
+		size_x / horizontal_fence_tile_size.x
+	) - 1
+
+func num_vertical_tiles(size_y):
+	return (
+		size_y / vertical_fence_tile_size.y
+	) - 1
+
+onready var nes = $NECornerFence.get_node("Sprite").get_region_rect().size
+onready var corner_fence_width_px = (
+nes.x / 2
+)
+onready var corner_fence_height_px = (
+nes.y
+)
+
+func procgen_fences(size):
+	print("procgen fence with size " + str(size))
+	for x in range(num_horizontal_tiles(size.x)):
+		var fence_top = horizontal_fence.instance()
+		add_child(fence_top)
+		fence_top.position = Vector2(
+			x * horizontal_fence_tile_size.x  + corner_fence_width_px,
+			0)
+		var fence_bottom = horizontal_fence.instance()
+		add_child(fence_bottom)
+		fence_bottom.position = Vector2(
+		x * horizontal_fence_tile_size.x + corner_fence_width_px,
+			size.y)
+	for y in range(num_vertical_tiles(size.y)):
+		var fence_left = vertical_fence.instance()
+		add_child(fence_left)
+		fence_left.position = Vector2(
+			0 ,
+			y * vertical_fence_tile_size.y + corner_fence_height_px )
+		var fence_right = vertical_fence.instance()
+		add_child(fence_right)
+		fence_right.position = Vector2(
+			size.x,
+			y * vertical_fence_tile_size.y  + corner_fence_height_px )
+
+func place_corners(size):
+	$NWCornerFence.position = Vector2(0,0)
+	$NECornerFence.position = Vector2(size.x,0)
+	$SWCornerFence.position = Vector2(0,size.y)
+	$SECornerFence.position = Vector2(size.x,size.y)
+	
+func init(sz: Vector2):
+	size = sz
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("autofence procgen with size " + str(size))
+	procgen_fences(size)
+	place_corners(size)
+	
+
---
+++ ProcFencedCow.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+### CAUTION!
+### This node needs to execute before the other procgen
+### nodes, as it is blissfully unaware of ProcZoneRepo's
+### capability of assigning it a suitable position in the
+### world.
+### See https://github.com/Terkwood/PLOWMAN/issues/35
+
+# adapted with inspiration from http://kidscancode.org/blog/2018/08/godot3_procgen1/
+const min_width  =  5   # in tiles
+const max_width  = 12   # in tiles
+const min_height =  5   # in tiles
+const max_height = 12   # in tiles
+const max_offset_x = Chunk.num_tiles_x - min_width  # in tiles
+const max_offset_y = Chunk.num_tiles_y - min_height # in tiles
+
+const NW = 6
+const NE = 8
+const SW = 12
+const SE = 14
+const HORIZONTAL = 1
+const VERTICAL = 4
+
+# get a reference to the map for convenience
+onready var Map = $TileMap
+onready var Set = Map.tile_set
+onready var Cow = $Animals/Cow
+
+onready var AutoFence = preload("res://AutoFence.tscn")
+
+func pixel_bounding_box(tile_offset_x,tile_offset_y,num_tiles_x,num_tiles_y):
+	return Rect2(
+		Vector2(tile_offset_x * Chunk.TILE_SIZE, tile_offset_y * Chunk.TILE_SIZE),
+		Vector2(num_tiles_x * Chunk.TILE_SIZE, num_tiles_y * Chunk.TILE_SIZE))
+
+func gen_fence_tiles():
+	var tile_offset_x = int(max(0, randi()%max_offset_x - min_width))
+	var tile_offset_y = int(max(0, randi()%max_offset_y - min_width))
+	
+	# in tiles
+	var num_tiles_x = int(min(Chunk.num_tiles_x - tile_offset_x, min_width + randi()%max_width))
+	var num_tiles_y = int(min(Chunk.num_tiles_y - tile_offset_y, min_height + randi()%max_height))
+	
+	return [tile_offset_x, tile_offset_y, num_tiles_x, num_tiles_y]
+	
+func place_cow(tile_offset_x, tile_offset_y, width, height):
+	Cow.position.x = (tile_offset_x + 2 + randi()%(int(max(1,width-3)))) * Chunk.TILE_SIZE
+	Cow.position.y = (tile_offset_y + 1 + randi()%(height-2)) * Chunk.TILE_SIZE
+	
+func animate_cow():
+	var available_anims = $Animals/Cow/Sprite/AnimationPlayer.get_animation_list()
+	$Animals/Cow/Sprite/AnimationPlayer.play(available_anims[randi()%available_anims.size()])
+
+func make_fence(bb):
+	var fence = AutoFence.instance()
+	fence.init(bb.size)
+	fence.position = bb.position
+	add_child(fence)
+	
+func _ready():
+	randomize()
+	var fc = gen_fence_tiles()
+	var tile_offset_x = fc[0]
+	var tile_offset_y = fc[1]
+	var num_tiles_x = fc[2]
+	var num_tiles_y = fc[3]
+	var bb = pixel_bounding_box(tile_offset_x,tile_offset_y,num_tiles_x,num_tiles_y)
+	place_cow(tile_offset_x, tile_offset_y, num_tiles_x, num_tiles_y)
+	animate_cow()
+	make_fence(bb)
+	ProcZoneRepo.try_add_proc_zone(bb)
