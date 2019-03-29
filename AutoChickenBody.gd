0e3dee531a40747733aaa90dfc3ca4d0f66c7e8d
Terkwood <metaterkhorn@gmail.com>
2019-03-29 19:00:26 -0400

    stub out chicken logic

--- AutoChickenBody.gd
+++ AutoChickenBody.gd
@@ -5,6 +5,7 @@ enum Movement { STOP, GO }
 
 var dir = Direction.E
 var mv = Movement.STOP
+var out_of_bounds = false
 
 var timestamp = 0
 const FREQ = 0.5
@@ -16,18 +17,17 @@ func _ready():
 	move_and_collide(Vector2(300,0))
 
 func move():
-	print("MOVE BIRD")
+	pass
 	
 func _physics_process(delta):
-	print("timestamp "+str(timestamp))
 	timestamp = timestamp + delta
 	if timestamp > FREQ:
 		move()
 		timestamp = 0
 		
-
 func _on_Area2D_body_exited(body: KinematicBody2D):
 	if body.get_instance_id() == get_instance_id():
-		print("cluck "+str(get_instance_id()))
 		dir = opposite(dir)
 		print("new dir " + str(dir))
+		mv = Movement.GO
+		out_of_bounds = true
