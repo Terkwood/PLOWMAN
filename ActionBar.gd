790a13b35c21520109241021611aad1a4ef11c79
Terkwood <metaterkhorn@gmail.com>
2019-03-30 14:19:12 -0400

    Make action bar stick to the bottom of the screen

---
+++ ActionBar.gd
@@ -0,0 +1,7 @@
+extends ItemList
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_item("turkey", $TurkeySample.texture)
+	add_item("turkey", $TurkeySample.texture)
+	add_item("turkey", $TurkeySample.texture)
