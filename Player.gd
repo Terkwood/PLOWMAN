7862aec6851b5a20c65490f7dc9f871b3d13b97f
Terkwood <metaterkhorn@gmail.com>
2019-03-29 21:24:02 -0400

    normalize player walk speed

--- Player.gd
+++ Player.gd
@@ -2,37 +2,37 @@ extends KinematicBody2D
 
 const WALK_SPEED = 225
 
-var velocity = Vector2()
+var dir = Vector2()
 
 func _physics_process(_delta):
 	var is_anim = false
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
+		dir.x = -1
 		$Sprite.get_node("WalkAnims").play("WalkLeft")
 		is_anim = true
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
+		dir.x =  1
 		$Sprite.get_node("WalkAnims").play("WalkRight")
 		is_anim = true
 	else:
-		velocity.x = 0
+		dir.x = 0
 		
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -WALK_SPEED
+		dir.y = -1
 		if !is_anim:
 			$Sprite.get_node("WalkAnims").play("WalkUp")
 		is_anim = true
 	elif Input.is_action_pressed("ui_down"):
-		velocity.y =  WALK_SPEED
+		dir.y =  1
 		if !is_anim:
 			$Sprite.get_node("WalkAnims").play("WalkDown")
 		is_anim = true
 	else:
-		velocity.y = 0
+		dir.y = 0
 	
 	if !is_anim:
 		$Sprite.get_node("WalkAnims").stop()
-	move_and_slide(velocity, Vector2(0, 0))
+	move_and_slide(dir.normalized() * WALK_SPEED, Vector2(0, 0))
 
 func _process(_delta):
 	ZIndex.hack(self.position.y, $Sprite, $Sprite)
