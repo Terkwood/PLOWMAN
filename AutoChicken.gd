244230595f9b200b25b3dca69ece13162a9d5cc0
Terkwood <metaterkhorn@gmail.com>
2019-03-30 08:39:36 -0400

    position chickens randomly

--- AutoChicken.gd
+++ AutoChicken.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 export var zone_size: Vector2 = Vector2(128,128)
+export var random_position = true
 
 func _ready():
+	if random_position:
+		position = Vector2(randi()%Chunk.width(), randi()%Chunk.height())
 	var collision_rect: RectangleShape2D = $Area2D/CollisionShape2D.shape
 	collision_rect.extents = zone_size
