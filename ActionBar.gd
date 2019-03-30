7f0500a47c53ec13eac15c441a852e5919ee19dd
Terkwood <metaterkhorn@gmail.com>
2019-03-30 14:29:15 -0400

    Add action bar UI

--- ActionBar.gd
+++ ActionBar.gd
@@ -1,7 +1,19 @@
 extends ItemList
 
+func add_samples():
+	add_item("  12", $PotatoIcon.texture)
+	add_item("   3", $CarrotIcon.texture)
+	add_item("   4", $ArtichokeIcon.texture)
+	add_item("    ", $TomatoIcon.texture)
+	add_item("   2", $ZucchiniIcon.texture)
+	add_item("    ", $CornIcon.texture)
+	add_item(" 444", $RedPepperIcon.texture)
+	add_item("    ", $PotatoIcon.texture)
+	add_item("    ", $CarrotIcon.texture)
+	add_item("    ", $ArtichokeIcon.texture)
+	add_item("   2", $CornIcon.texture)
+	add_item("  11", $RedPepperIcon.texture)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_item("turkey", $TurkeySample.texture)
-	add_item("turkey", $TurkeySample.texture)
-	add_item("turkey", $TurkeySample.texture)
+	pass
