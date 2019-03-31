d31c17fa0c1de2511464e329b2c543b4aff9499e
Terkwood <metaterkhorn@gmail.com>
2019-03-31 17:50:42 -0400

    cleanup

--- PickupArea.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.connect("body_entered", self, "_on_PickupArea_body_entered")
-
-func _on_PickupArea_body_entered(body: KinematicBody2D):
-	if body:
-		print("Welcome kinematic friend: %s" % body.name)
