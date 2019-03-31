db6e03207b54ca8d14635d1999771c036f61a18e
Terkwood <metaterkhorn@gmail.com>
2019-03-31 18:49:31 -0400

    push and pop candidate bodies

--- Player.gd
+++ Player.gd
@@ -66,13 +66,17 @@ func _process(_delta):
 	ZIndex.hack(self.position.y, $Sprite, $Sprite)
 
 
-var pickup_candidates = []
-
+var pickup_candidate_bodies = []
 
 func _on_ReachArea_body_entered(body):
 	print("Pickup area: %s" % body.name)
 	var has_pickup_manager = body.has_node("PickupManager")
 	if has_pickup_manager:
 		print("Target has a PickupManager")
-		pickup_candidates.push_front(body.get_node("PickupManager"))
+		pickup_candidate_bodies.push_front(body)
 
+func _on_ReachArea_body_exited(body):
+	for c in range(pickup_candidate_bodies.size()):
+		if pickup_candidate_bodies[c].get_instance_id() == body.get_instance_id():
+			pickup_candidate_bodies.remove(c)
+			print("removed candidate body %s" % body.name)
