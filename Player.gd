8083b37133d1a70b0fe20c59536b97789b7d83c8
Terkwood <metaterkhorn@gmail.com>
2019-03-31 20:44:54 -0400

    move functions about

--- Player.gd
+++ Player.gd
@@ -42,3 +42,18 @@ func _process(_delta):
 func _unhandled_input(event):
 	if Input.is_action_pressed("game_interact"):
 		print(".")
+
+var pickup_candidate_bodies = []
+
+func _on_ReachArea_body_entered(body):
+	print("Pickup area: %s" % body.name)
+	var has_pickup_manager = body.has_node("PickupManager")
+	if has_pickup_manager:
+		print("Target has a PickupManager")
+		pickup_candidate_bodies.push_front(body)
+
+func _on_ReachArea_body_exited(body):
+	for c in range(pickup_candidate_bodies.size()):
+		if pickup_candidate_bodies[c].get_instance_id() == body.get_instance_id():
+			pickup_candidate_bodies.remove(c)
+			print("removed candidate body %s" % body.name)
