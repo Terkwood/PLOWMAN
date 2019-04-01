709d7c8a2d5af8e768e66fb2b580aa3e71825c2c
Terkwood <metaterkhorn@gmail.com>
2019-03-31 23:41:28 -0400

    Add FIXME

--- PickupManager.gd
+++ PickupManager.gd
@@ -2,6 +2,13 @@ extends Node
 
 export var target: NodePath
 
+# WARNING! FIXME!
+# This item name must match an
+# entry in Inventory.gd,
+# or it won't end up correctly
+# linked to a TextureRect icon
+# if it's drawn in the ActionBar
+# See https://github.com/Terkwood/PLOWMAN/issues/46
 export var item_name: String
 
 func destroy_target():
