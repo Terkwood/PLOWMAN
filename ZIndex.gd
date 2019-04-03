8b23dff2ab3b936ebcf31bf1031a49500258711d
Terkwood <metaterkhorn@gmail.com>
2019-04-03 19:42:11 -0400

    hack the hack for z-index

--- ZIndex.gd
+++ ZIndex.gd
@@ -1,5 +1,10 @@
 extends Node
 
+const RATE = 0.25
+
 func hack(s: Sprite):
+	var az = s.get("actual_z_index")
+	var actual_z_index = az if az else 0.0
+
 	var g = s.get_global_transform().get_origin().y + s.region_rect.size.y / 2 * s.scale.y
-	s.z_index=g
+	s.z_index=g * RATE + actual_z_index
