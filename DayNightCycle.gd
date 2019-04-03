25838cb84d86a9192f643a5b200bbdd2f8898224
Terkwood <metaterkhorn@gmail.com>
2019-04-02 20:56:13 -0400

    Fix comparison logic, refactor consts

--- DayNightCycle.gd
+++ DayNightCycle.gd
@@ -5,46 +5,47 @@ const day_duration_real_seconds = 60 * 60 * day_duration_minutes
 
 enum { NIGHT, DAWN, DAY, DUSK }
 
-const PERIOD_START = {
+# 24 hour time (0-23)
+const HOURS = {
 	NIGHT: 22,
 	DAWN: 5,
 	DAY: 8,
 	DUSK: 19,
 }
 
-const game_start_hour = 5 # 24 hours time (0-23)
-var day_start_number = 1
+const game_start_hour = 5
+
+const day_start_number = 1
 
 var color_dawn = Color(0.86, 0.70, 0.70, 1.0)
 var color_day = Color(1.0, 1.0, 1.0, 1.0)
 var color_dusk = Color(0.59, 0.66, 0.78, 1.0)
 var color_night = Color(0.07, 0.09, 0.38, 1.0)
 
-
 var current_time
 var current_day_hour
 var current_day_number
 
-var transition_duration
-var transition_duration_time = 1 # In hours
+const transition_duration_game_hours = 1 # In hours
+const transition_duration_real_secs = (((day_duration_real_seconds / 24) * transition_duration_game_hours) / 60)
 
 var period
 
 func is_night():
-	return current_day_hour >= PERIOD_START[NIGHT] or current_day_hour <= PERIOD_START[DAWN]
+	return current_day_hour >= HOURS[NIGHT] or current_day_hour < HOURS[DAWN]
 func is_dawn():
-	return current_day_hour >= PERIOD_START[DAWN] and current_day_hour <= PERIOD_START[DAY]
+	return current_day_hour >= HOURS[DAWN] and current_day_hour < HOURS[DAY]
 func is_day():
-	return current_day_hour >= PERIOD_START[DAY] and current_day_hour <= PERIOD_START[DUSK]
+	return current_day_hour >= HOURS[DAY] and current_day_hour < HOURS[DUSK]
 func is_dusk():
-	return current_day_hour >= PERIOD_START[DUSK] and current_day_hour <= PERIOD_START[NIGHT]
+	return current_day_hour >= HOURS[DUSK] and current_day_hour < HOURS[NIGHT]
 
 func _ready():
+	print("Transition duration (game hours): %f" % transition_duration_game_hours)
+	print("Transition duration (real  secs): %f" % transition_duration_real_secs)
 	current_day_number = day_start_number
 	current_time = (day_duration_real_seconds / 24) * game_start_hour
 	current_day_hour = current_time / (day_duration_real_seconds / 24)
-	
-	transition_duration = (((day_duration_real_seconds / 24) * transition_duration_time) / 60)
 	
 	if is_night():
 		period = NIGHT
@@ -84,17 +85,17 @@ func update_period(new_period):
 		period = new_period
 
 		if period == NIGHT:
-			$Tween.interpolate_property(self, "color", color_dusk, color_night, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.interpolate_property(self, "color", color_dusk, color_night, transition_duration_real_secs, Tween.TRANS_SINE, Tween.EASE_OUT)
 			$Tween.start()
 			
 		if period == DAWN:
-			$Tween.interpolate_property(self, "color", color_night, color_dawn, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.interpolate_property(self, "color", color_night, color_dawn, transition_duration_real_secs, Tween.TRANS_SINE, Tween.EASE_OUT)
 			$Tween.start()
 	
 		if period == DAY:
-			$Tween.interpolate_property(self, "color", color_dawn, color_day, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.interpolate_property(self, "color", color_dawn, color_day, transition_duration_real_secs, Tween.TRANS_SINE, Tween.EASE_OUT)
 			$Tween.start()
 	
 		if period == DUSK:
-			$Tween.interpolate_property(self, "color", color_day, color_dusk, transition_duration, Tween.TRANS_SINE, Tween.EASE_OUT)
+			$Tween.interpolate_property(self, "color", color_day, color_dusk, transition_duration_real_secs, Tween.TRANS_SINE, Tween.EASE_OUT)
 			$Tween.start()
