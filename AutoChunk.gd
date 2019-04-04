5d1ccb5260b4f5dfda46f5f9b75b2b236af74283
Terkwood <metaterkhorn@gmail.com>
2019-04-04 19:40:48 -0400

    delete manual placements

--- AutoChunk.gd
+++ AutoChunk.gd
@@ -9,10 +9,6 @@ const ProcPonds = preload("res://ProcPonds.tscn")
 
 var chunk_id = null
 
-# cheater tips https://godotengine.org/qa/24745/how-to-check-type-of-a-custom-class
-func is_class(c): return c == "AutoChunk" or .is_class(c)
-func get_class(): return "AutoChunk"
-
 func _init(chunk_id: Vector2):
 	self.chunk_id = chunk_id
 	self.position = Vector2(
