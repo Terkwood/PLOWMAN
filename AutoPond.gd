da16df7bae92dadd6fd60e3c7ef5276745877527
Terkwood <metaterkhorn@gmail.com>
2019-04-04 14:24:02 -0400

    place pond borders

--- AutoPond.gd
+++ AutoPond.gd
@@ -14,12 +14,16 @@ const E_BORDER_TILE = "grass_water_edge_e"
 const S_BORDER_TILE = "grass_water_edge_s"
 const W_BORDER_TILE = "grass_water_edge_w"
 
+func set_cell_size():
+	$TileMap.set_cell_size(Vector2(Chunk.TILE_SIZE, Chunk.TILE_SIZE))
+
 # Obey chunk tile sizing
 func snap_size():
 	size = Vector2(floor(size.x / Chunk.TILE_SIZE) * Chunk.TILE_SIZE,
 					floor(size.y / Chunk.TILE_SIZE) * Chunk.TILE_SIZE)
 
 func place():
+	set_cell_size()
 	snap_size()
 	
 	var zone: Rect2 = ProcZoneRepo.assign_zone(size)
@@ -29,21 +33,34 @@ func place():
 	var ty = zone.size.y - int(ceil(zone.size.y)) % Chunk.TILE_SIZE
 	var trimmed_size = Vector2(tx, ty)
 								 
-	var num_tiles_x = int(trimmed_size.x / Chunk.TILE_SIZE)
-	var num_tiles_y = int(trimmed_size.y / Chunk.TILE_SIZE)
+	var num_tiles_x = int(max(0,trimmed_size.x / Chunk.TILE_SIZE) - 1)
+	var num_tiles_y = int(max(0,trimmed_size.y / Chunk.TILE_SIZE) - 1)
 	
 	# Place corners
 	$TileMap.set_cellv(Vector2(0, 0),
 						$TileMap.tile_set.find_tile_by_name(NW_CORNER_TILE))
 
-	$TileMap.set_cellv(Vector2(num_tiles_x, 0),
+	$TileMap.set_cellv(Vector2(num_tiles_x , 0),
 						$TileMap.tile_set.find_tile_by_name(NE_CORNER_TILE))
 	
-	$TileMap.set_cellv(Vector2(0, num_tiles_y),
+	$TileMap.set_cellv(Vector2(0, num_tiles_y ),
 						$TileMap.tile_set.find_tile_by_name(SW_CORNER_TILE))
 	
-	$TileMap.set_cellv(Vector2(num_tiles_x, num_tiles_y),
+	$TileMap.set_cellv(Vector2(num_tiles_x , num_tiles_y ),
 						$TileMap.tile_set.find_tile_by_name(SE_CORNER_TILE))
+
+	var w_border = $TileMap.tile_set.find_tile_by_name(W_BORDER_TILE)
+	var e_border = $TileMap.tile_set.find_tile_by_name(E_BORDER_TILE)
+	var n_border = $TileMap.tile_set.find_tile_by_name(N_BORDER_TILE)
+	var s_border = $TileMap.tile_set.find_tile_by_name(S_BORDER_TILE)
+	
+	for x in range(max(0, num_tiles_x - 1)):
+		$TileMap.set_cellv(Vector2(x + 1, 0), n_border)
+		$TileMap.set_cellv(Vector2(x + 1, num_tiles_y), s_border)
+
+	for y in range(max(0, num_tiles_y - 1)):
+		$TileMap.set_cellv(Vector2(0, y + 1), w_border)
+		$TileMap.set_cellv(Vector2(num_tiles_x, y + 1), e_border)
 
 func _ready():
 	if !delay_placement:
