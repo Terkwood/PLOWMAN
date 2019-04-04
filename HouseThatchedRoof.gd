4a00b24ff69b46d2ffa067ac36b994b3b18a7948
Terkwood <metaterkhorn@gmail.com>
2019-04-04 16:35:52 -0400

    nits

--- HouseThatchedRoof.gd
+++ HouseThatchedRoof.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 var size = Vector2(196,196)
 
+onready var chunk_id = Chunk.id(self)
+
 func _ready():
 	var zone = ProcZoneRepo.assign_zone(size)
 	self.position = zone.position
 	print("house parent is %s" % get_parent())
-	print("house chunk_id is %d" % Chunk.id(self))
-	print("post-set parent chunk_id is  %d " % Chunk.id(get_parent()))
-	print("post-set 2 parent chunk_id is  %d " % Chunk.id(get_parent()))
+	print("my chunk id is %d" % chunk_id)
+	$Torch2Body.set("chunk_id",Chunk.id($Torch2Body))
+	print("torch chunk id is %s" % $Torch2Body.get("chunk_id"))
