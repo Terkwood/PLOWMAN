c68599c39291b3109ac5c9d965437e4fa3979784
Terkwood <metaterkhorn@gmail.com>
2019-04-06 13:07:09 -0400

    tune consts

--- AutoChunk.gd
+++ AutoChunk.gd
@@ -14,13 +14,13 @@ onready var player = $"/root/ProcFarm".find_node("Player",true)
 
 const NUM_CHICKENS = 4
 const PLANT_SIZES = [
+		Vector2(512,1024),
+		Vector2(1024,512),
 		Vector2(512,768),
-		Vector2(768,512),
-		Vector2(512,768),
-		Vector2(512,256),
-		Vector2(256,512)
+		Vector2(768,256),
+		Vector2(384,384)
 	]
-const NUM_PONDS = 1
+const NUM_PONDS = 2
 const DRAW_HOUSE = true
 const DRAW_FENCED_COW = true
 
@@ -41,6 +41,13 @@ func init(chunk_id: Vector2):
 	add_child(field)
 	field.set_owner(self) # set owner so that resource saving works, https://godotengine.org/qa/903/how-to-save-a-scene-at-run-time
 
+	# YOU MUST DRAW THE FENCED COW FIRST
+	# Because ProcFencedCow does not respect other nodes' proc zones
+	if DRAW_FENCED_COW:
+		var fenced_cow = ProcFencedCow.instance()
+		add_child(fenced_cow)
+		fenced_cow.set_owner(self) # set owner so that resource saving works
+
 	for i in range(NUM_CHICKENS):
 		var cluck = AutoChicken.instance()
 		cluck.zone_size = Vector2(32,32)
@@ -57,11 +64,6 @@ func init(chunk_id: Vector2):
 	ponds.num_ponds = NUM_PONDS
 	add_child(ponds)
 	ponds.set_owner(self) # set owner so that resource saving works
-
-	if DRAW_FENCED_COW:
-		var fenced_cow = ProcFencedCow.instance()
-		add_child(fenced_cow)
-		fenced_cow.set_owner(self) # set owner so that resource saving works
 
 	if DRAW_HOUSE:
 		var house = HouseThatchedRoof.instance()
