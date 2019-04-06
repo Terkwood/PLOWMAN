dae18625cca7b2b7274d22490f05d96acf6660a3
Terkwood <metaterkhorn@gmail.com>
2019-04-05 21:12:51 -0400

    use deferred calls

--- ProcChunks.gd
+++ ProcChunks.gd
@@ -24,20 +24,15 @@ func _ready():
 func _on_player_entered_chunk(chunk_id: Vector2):
 	print("player entered chunk %s" % chunk_id)
 	$ChunkLabel.set_text("%s" % chunk_id)
-	#print("active chunks: %s" % str(active_chunks.keys()))
-	var to_erase = []
 	for i in active_chunks:
 		if (
 			i.x < chunk_id.x - 1 || i.x > chunk_id.x + 1 ||
 			i.y < chunk_id.y - 1 || i.y > chunk_id.y + 1
 		):
 			var cr = active_chunks[i]
-			var chunk = cr.chunk
-			storage.save_scene(chunk, cr["storage_name"])
-			remove_child(chunk)
-			to_erase.push_front(i)
-			stored_chunks[i] = cr["storage_name"]
-			print("saved to disk: %s" % cr["storage_name"])
+			if !_pend_save.has(i):
+				_pend_save[i] = cr
+				call_deferred("save_chunk", cr, i)
 
 		var adjacents = [
 			Vector2(1,0),
@@ -54,11 +49,30 @@ func _on_player_entered_chunk(chunk_id: Vector2):
 			var cid_a = chunk_id + a
 			if !active_chunks.has(cid_a) && stored_chunks.has(cid_a):
 				var file = stored_chunks[cid_a]
-				var chunk = storage.load_scene(file)
-				add_child(chunk)
-				chunk.set_owner(get_parent()) # set owner so that resource saving works
-				stored_chunks.erase(file)
-				active_chunks[cid_a] = {"chunk":chunk,"storage_name":file}
+				# cannot create area2d when another area2d is undergoing
+				# physics processing.  this will defer the execution
+				# until "idle time"
+				if !_pend_restore.has(cid_a):
+					_pend_restore[cid_a] = file
+					call_deferred("restore_chunk", file, cid_a)
 
-	for e in to_erase:
-		active_chunks.erase(e)	
+# dict prevents multiple calls at the same time
+var _pend_save = {}
+func save_chunk(cr, chunk_id: Vector2):
+	var chunk = cr.chunk
+	storage.save_scene(chunk, cr["storage_name"])
+	remove_child(chunk)
+	active_chunks.erase(chunk_id)
+	stored_chunks[chunk_id] = cr["storage_name"]
+	_pend_save.erase(chunk_id)
+	print("saved to disk: %s" % cr["storage_name"])
+
+# dict prevents multiple calls at the same time
+var _pend_restore = {}
+func restore_chunk(file: String, chunk_id: Vector2):
+	var chunk = storage.load_scene(file)
+	add_child(chunk)
+	chunk.set_owner(get_parent()) # set owner so that resource saving works
+	stored_chunks.erase(file)
+	active_chunks[chunk_id] = {"chunk":chunk,"storage_name":file}
+	_pend_restore.erase(chunk_id)
