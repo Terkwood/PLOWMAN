933d2c51eb001764c80d128fec768ce072f458b6
Terkwood <metaterkhorn@gmail.com>
2019-04-06 20:09:17 -0400

    user dir

--- SceneStorage.gd
+++ SceneStorage.gd
@@ -2,14 +2,20 @@ extends Node
 
 const OUTPUT_FOLDER = "proc_packed"
 
+func _ready():
+	var dir = Directory.new()
+	dir.open("user://")
+	dir.make_dir(OUTPUT_FOLDER)
+	dir.close()
+
 func save_scene(node: Node, name: String):
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(node)
-	var file = "res://%s/%s.tscn" % [OUTPUT_FOLDER,name]
+	var file = "user://%s.tscn" % name
 	ResourceSaver.save(file, packed_scene)
 	
 func load_scene(file_name: String):
-	var packed_scene = load("res://%s/%s.tscn" % [OUTPUT_FOLDER,file_name])
+	var packed_scene = load("user://%s.tscn" % file_name)
 	var i = packed_scene.instance()
 	print("loaded %s " % file_name)
 	return i
