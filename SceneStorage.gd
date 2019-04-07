d6b2c507c26b0144525f732441f7b2caa8372309
Terkwood <metaterkhorn@gmail.com>
2019-04-06 20:12:07 -0400

    trim

--- SceneStorage.gd
+++ SceneStorage.gd
@@ -1,14 +1,5 @@
 extends Node
 
-#FIXME
-#const OUTPUT_FOLDER = "proc_packed"
-
-#func _ready():
-#	var dir = Directory.new()
-#	dir.open("user://")
-#	dir.make_dir(OUTPUT_FOLDER)
-#	dir.close()
-
 func save_scene(node: Node, name: String):
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(node)
@@ -18,5 +9,4 @@ func save_scene(node: Node, name: String):
 func load_scene(file_name: String):
 	var packed_scene = load("user://%s.tscn" % file_name)
 	var i = packed_scene.instance()
-	print("loaded %s " % file_name)
 	return i
