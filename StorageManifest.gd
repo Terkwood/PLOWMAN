b205d874b1e9412ec1a20f34aeb6b3d085eebecc
Terkwood <metaterkhorn@gmail.com>
2019-04-07 11:12:24 -0400

    trim path names

--- StorageManifest.gd
+++ StorageManifest.gd
@@ -11,11 +11,26 @@ static func merge_dir(target, patch):
 		else:
 		    target[key] = patch[key]
 
+const PATH_MATCH = "*AutoChunk*"
+static func trim_path(p: NodePath):
+	var idx = 0
+	var nc = p.get_name_count()
+	for x in range(nc):
+		if p.get_name(x).match (PATH_MATCH):
+			idx = x
+	var trimmed = ""
+	var lim = nc - idx
+	for y in range(lim):
+		trimmed += p.get_name(y + idx)
+		if y != lim - 1:
+			trimmed += "/"
+	return NodePath(trimmed)
+
 ## Generate a storage manifest for a given type of node
 ## It should return a dictionary with reasonably-formatted
 ## information on the position of landmarks, etc.
 static func generate(node: Node, accum: Dictionary = {}):
-	var path = node.get_path()
+	var path = trim_path(node.get_path())
 
 	if node.has_method("manifest"):
 		accum[path] = node.call("manifest")
@@ -25,6 +40,8 @@ static func generate(node: Node, accum: Dictionary = {}):
 		merge_dir(accum, child_accum)
 	
 	return accum
+
+
 
 static func position_manifest(node: Node):
 	return { "position": node.position }
