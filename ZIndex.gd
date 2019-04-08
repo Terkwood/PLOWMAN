d57413bf5588a3a6c4b47743ce3116a23ded771d
Terkwood <metaterkhorn@gmail.com>
2019-04-08 19:11:56 -0400

    tweak

--- ZIndex.gd
+++ ZIndex.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const RATE = 0.125
+const _RATE = 0.125
 
 func hack(s: Sprite):
 	# You can set the "actual z index", if you want!
@@ -8,7 +8,7 @@ func hack(s: Sprite):
 	var actual_z_index = az if az else 0.0
 
 	var g = s.get_global_transform().get_origin().y + s.region_rect.size.y / 2 * s.scale.y
-	var h = g * RATE + actual_z_index
+	var h = g * _RATE + actual_z_index
 	if h < 0 && randi()%10 <3:
 		print("hack %f" %  h )
 	s.z_index = h
