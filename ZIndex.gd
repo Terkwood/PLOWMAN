3197ba00a298d3e0ab55ac11bacf64a039cdcfb2
Terkwood <metaterkhorn@gmail.com>
2019-04-08 19:27:35 -0400

    move z-index hacks hack

--- ZIndex.gd
+++ ZIndex.gd
@@ -5,7 +5,9 @@ const _GR = 2000
 const _M = _GR * _GR
 
 func hack(s: Sprite):
-	# You can set the "actual z index", if you want!
+	# This property will be added to the total, below.
+	# Potentially useful if you need to put a cup of
+	# coffee on the table.
 	var az = s.get("actual_z_index")
 	var actual_z_index = az if az else 0.0
 
@@ -13,8 +15,8 @@ func hack(s: Sprite):
 	# that z-index is reasonable in negatively-numbered
 	# position.y values
 	var g = s.get_global_transform().get_origin().y + s.region_rect.size.y / 2 * s.scale.y
-	var h = g * _RATE + actual_z_index
+	var h = g * _RATE
 	var i = int(h * _GR) if h >= 0 else int((_GR - abs(h)) * _GR)
-	var j: float = (i % _M) / _GR
+	var j: float = (i % _M) / _GR + actual_z_index
 	s.z_index = j
 	s.z_as_relative = false
